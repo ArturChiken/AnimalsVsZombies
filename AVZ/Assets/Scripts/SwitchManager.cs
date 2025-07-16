ecd0387472dadae006c2d04889667a5beb02890d
Renat Stepanov <srvowo@gmail.com>
2025-07-16 16:15:22 +0300

    version 1.2
    rework sceneswitcher.cs + added scene(tests)

--- AVZ/Assets/Scripts/SwitchManager.cs
+++ AVZ/Assets/Scripts/SwitchManager.cs
@@ -1,22 +1,49 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SwitchManager : MonoBehaviour
 {
-    [SerializeField] int gameSceneIndex = 1;
+    public static SwitchManager instance;
+
+    [SerializeField] int MainMenuIndex = 0;
+    [SerializeField] int LevelSelectorIndex = 1;
+    [SerializeField] int InfLevelIndex = 10;
+
+    public float timeForWait = 2;
+
+    bool isSwitching;
 
     public void AdvPlayButton()
     {
-        SceneSwitch(gameSceneIndex);
+        SceneSwitch(LevelSelectorIndex);
     }
 
     public void InfPlayButton()
     {
-        SceneSwitch(gameSceneIndex);
+        SceneSwitch(InfLevelIndex);
     }
 
+    public void MainMenuButton()
+    {
+        SceneSwitch(MainMenuIndex);
+    }
     public void SceneSwitch(int index)
     {
+        if (isSwitching)
+        {
+            return;
+        }
+        isSwitching = true;
+
+        StartCoroutine(SceneSwitchTimer(index));
+    }
+
+    IEnumerator SceneSwitchTimer(int index)
+    {
+        yield return new WaitForSeconds(timeForWait);
+        isSwitching = false;
         SceneManager.LoadScene(index);
     }
 }
