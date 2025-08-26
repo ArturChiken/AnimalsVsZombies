888fb70e96a613c174ee495b79f5bdd098c75572
Artur Yakimenko <ponygangster@gmail.com>
2025-08-27 01:36:00 +0300

    all roads done... i think

--- AVZ/Assets/Scripts/Tile.cs
+++ AVZ/Assets/Scripts/Tile.cs
@@ -1,6 +1,29 @@
+using NUnit.Framework.Internal.Filters;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Tile : MonoBehaviour
 {
     public bool hasAnimal = false;
+    public int[] actNums;
+    public int[] lvlNums;
+
+    private void Start()
+    {
+        gameObject.SetActive(true);
+
+        foreach (int act in actNums)
+        {
+            if (act == ActMenuButtonsManager.currAct)
+            {
+                foreach(int lvl in lvlNums)
+                {
+                    if (lvl == LevelMenuButtonManager.currLevel)
+                    {
+                        gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
 }
