b1fe735839425c2f47211bf4bcd8cc06b42841f8
Artur Yakimenko <ponygangster@gmail.com>
2025-09-12 00:24:45 +0300

    added rewarded adv

---
+++ AVZ/Assets/Scripts/KillBoxInLevel.cs
@@ -0,0 +1,37 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class KillBoxInLevel : MonoBehaviour
+{
+    private Gamemanager gameManager;
+    public GameObject bomb;
+    private bool isNeedRevive = false;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("Gamemanager").GetComponent<Gamemanager>();
+    }
+
+    private void Update()
+    {
+        if (!gameManager.isGameStarted)
+        {
+            isNeedRevive = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isNeedRevive)
+        {
+            isNeedRevive = false;
+            DropBomb(collision);
+            collision.GetComponent<Labubu>().LabubuGetHit(999);
+        }
+    }
+
+    private void DropBomb(Collider2D collision)
+    {
+        GameObject myBomb = Instantiate(bomb, collision.GetComponent<Labubu>().transform.position, Quaternion.identity);
+    }
+}
